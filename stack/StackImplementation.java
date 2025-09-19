89c2393b82746eebfb9d62dc0bcbe99dc784ffae
Kilari Meenakshi <kilariaruna17@gmail.com>
2025-09-19 13:54:27 +0530

    Update StackImplementation.java

--- stack/StackImplementation.java
+++ stack/StackImplementation.java
@@ -3,14 +3,14 @@ package stack;
 public class StackImplementation {
     public static void main(String[] args) {
         Stack1 st = new Stack1(5);
-
         st.push(10);
         st.push(20);
         st.push(30);
-
-        System.out.println("Top element: " + st.peek());
-        System.out.println("Popped: " + st.pop());
-        System.out.println("Size: " + st.size());
+        st.push(31);
+        System.out.println("Highest element: " + st.peek());
+        System.out.println("Popped element: " + st.pop());
+        System.out.println("Stack size: " + st.size());
         System.out.println("Search 10: " + st.search(10));
     }
 }
+
